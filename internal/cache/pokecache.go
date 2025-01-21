5a9c9b6b09bf72c7e2447d7a8aa7beb118be42ee
Aman-Shetty <amanshetty370@gmail.com>
2025-01-21 20:02:12 +0530

    Added mutex in cache

--- internal/cache/pokecache.go
+++ internal/cache/pokecache.go
@@ -1,9 +1,13 @@
 package cache
 
-import "time"
+import (
+	"sync"
+	"time"
+)
 
 type Cache struct {
 	cache map[string]CacheEntry
+	mux *sync.Mutex
 }
 
 type CacheEntry struct{
@@ -14,12 +18,15 @@ type CacheEntry struct{
 func NewCache(interval time.Duration) Cache{
 	c := Cache{
 		cache: make(map[string]CacheEntry),
+		mux: &sync.Mutex{},
 	}
 	go c.reapLoop(interval)
 	return c
 }
 
 func (c *Cache) Add(key string, val []byte) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	c.cache[key] = CacheEntry{
 		val: val,
 		createdAt: time.Now().UTC(),
@@ -27,11 +34,15 @@ func (c *Cache) Add(key string, val []byte) {
 }
 
 func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	cacheEntry, ok := c.cache[key]
 	return cacheEntry.val, ok
 }
 
 func (c *Cache) reap(interval time.Duration) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
 	MinutesAgo := time.Now().UTC().Add(-interval)
 	for key, value := range c.cache {
 		if value.createdAt.Before(MinutesAgo) {
